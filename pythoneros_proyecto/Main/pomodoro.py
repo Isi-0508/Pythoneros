37ecb792277e32cfffd2ad86f9faee98244f3ca5
Martin-onate <monate.tapia@gmail.com>
2025-10-25 20:27:42 -0300

    sesiones (aun en proseso)

--- pythoneros_proyecto/Main/pomodoro.py
+++ pythoneros_proyecto/Main/pomodoro.py
@@ -10,3 +10,16 @@ def temporizador_pomodoro(duracion_minutos, callback=None):
             callback()
     hilo = threading.Thread(target=run)
     hilo.start()
+    return hilo
+
+sesiones_restantes = [1, 1]
+def siguiente_sesion():
+    if sesiones_restantes:
+        duracion = sesiones_restantes.pop(0)
+        temporizador_pomodoro(duracion, callback=siguiente_sesion)
+    else:
+        print("🎉 ¡Excelente, ciclo completado!")
+
+temporizador_pomodoro(0.1, callback=siguiente_sesion)
+print("El Pomodoro está corriendo en segundo plano")
+time.sleep(0.2 * 60)
