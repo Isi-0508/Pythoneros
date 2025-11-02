762279b74088bfc337caf5bd27f1e3604dabba23
laura73535 <lorellanaa@usm.cl>
2025-11-02 18:43:19 -0300

    Primer tema casi listo, solo falta los dropdowns

--- pythoneros_proyecto/pythoneros_proyecto/settings.py
+++ pythoneros_proyecto/pythoneros_proyecto/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -121,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
+    ,
 ]
 
 MEDIA_URL = '/media/'
