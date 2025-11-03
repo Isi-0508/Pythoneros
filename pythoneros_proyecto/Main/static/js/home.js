f88df4bfe78d28e12bc5206118afcfda5da60bf5
marshall-orders-jr <Relseiki@gmail.com>
2025-11-03 19:51:51 -0300

    'errordeaudiosolucionado'

--- pythoneros_proyecto/Main/static/js/home.js
+++ pythoneros_proyecto/Main/static/js/home.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".dropdown-item[data-tema]");
+  const links = document.querySelectorAll('data-tema');
   const body = document.body;
   const temaGuardado = localStorage.getItem("tema");
   if (temaGuardado) {
     body.className = temaGuardado;
-  }
-
-  links.forEach(link => {
+  } links.forEach(link => {
     link.addEventListener("click", e => {
       e.preventDefault();
       const tema = e.target.getAttribute("data-tema");
-      body.className = tema; 
+      body.className = tema;
       
       localStorage.setItem("tema", tema);
     });
